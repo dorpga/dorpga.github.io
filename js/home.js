b9bfaf4738b950e4a98105f8ed18a3ee00dde1af
Carver Harrison <pcarverb@hotmail.com>
2017-12-19 12:45:21 -0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -3,9 +3,9 @@ document.querySelectorAll('.CategoryButton').forEach(function(i) {
 		var c = i.getAttribute('data-value')
 		document.querySelectorAll('.PostBox').forEach(function(j) {
 			if (j.getAttribute('data-category') == c) {
-				j.style.visibility = 'visible';
+				j.style.display = 'initial';
 			} else {
-				j.style.visibility = 'hidden';
+				j.style.display = 'none';
 			}
 		});
 	})
